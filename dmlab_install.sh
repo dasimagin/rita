552056e804ae3f91b8414e990ed8f9c95bb6d6a5
Sokolov Roman <sokolov5roma@mail.ru>
2019-03-31 13:52:11 +0300

    + dmlab install

--- dmlab_install.sh
+++ dmlab_install.sh
@@ -19,7 +19,27 @@ cc_library(
     visibility = [\"//visibility:public\"],
 )
 " > python.BUILD
+cd python/pip_package
+echo "
+import setuptools
 
+REQUIRED_PACKAGES = [
+    'numpy >= 1.13.3',
+    'six >= 1.10.0',
+]
+
+setuptools.setup(
+    name='DeepMind Lab',
+    version='1.0',
+    description='DeepMind Lab',
+    long_description='',
+    url='https://github.com/deepmind/lab',
+    author='DeepMind',
+    packages=setuptools.find_packages(),
+    install_requires=REQUIRED_PACKAGES,
+    include_package_data=True)
+" > setup.py
+cd ../..
 
 bazel build --python_path=/usr/bin/python3 -c opt python/pip_package:build_pip_package
 ./bazel-bin/python/pip_package/build_pip_package /tmp/dmlab_pkg
