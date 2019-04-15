293dd92eaac66f63d7741007bdd524754af72a2a
granilace <vakukanov@edu.hse.ru>
2019-04-15 12:46:53 +0000

    Add curiosity reward normalizing, change CNN architecture in curiosity-CNN

--- curiosity/model.py
+++ curiosity/model.py
@@ -10,9 +10,12 @@ class CuriosityRewarder(nn.Module):
         super(CuriosityRewarder, self).__init__()
         self.n_actions = n_actions
         self.conv_features = SimpleConvNet(observation_shape)
+        self.mean = 0
+        self.mean_sq = 1
         
-        self.fc1 = nn.Linear(self.conv_features.cnn_output_size + n_actions, 400)
-        self.fc2 = nn.Linear(400 + n_actions, self.conv_features.cnn_output_size)
+        self.fc1 = nn.Linear(self.conv_features.cnn_output_size + n_actions, 300)
+        self.fc2 = nn.Linear(300 + n_actions, 800)
+        self.fc3 = nn.Linear(800 + n_actions, self.conv_features.cnn_output_size)
         
     def forward(self, state, action_oh):
         with torch.no_grad():
@@ -21,6 +24,8 @@ class CuriosityRewarder(nn.Module):
         x = F.relu(self.fc1(x))
         x = torch.cat([x, action_oh], dim=1)
         x = F.relu(self.fc2(x))
+        x = torch.cat([x, action_oh], dim=1)
+        x = F.relu(self.fc3(x))
         return x
         
     def get_reward(self, state, action, next_state):
@@ -31,6 +36,9 @@ class CuriosityRewarder(nn.Module):
         with torch.no_grad():
             real_next_state = self.conv_features(next_state)
         loss = torch.sum((predicted_next_state - real_next_state.detach())**2)
-        return loss
+        self.mean = 0.99 * self.mean + 0.01 * loss.detach()
+        self.mean_sq = 0.99 * self.mean_sq + 0.01 * (loss.detach()**2)
+        var = self.mean_sq - self.mean**2
+        return (loss - self.mean) / (var**0.5)
         
         
