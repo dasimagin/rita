6e093bdc0efb1f0487fb12cec618289781783abd
sokolov roman <sokolov5roma@mail.ru>
2019-05-24 14:50:29 +0300

    change installation

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -1,9 +1,9 @@
-apt-get update
-apt-get -y upgrade
-apt-get install -y cmake python3-pip gettext
-apt-get install -y pkg-config zip g++ zlib1g-dev unzip python3 python
-apt-get install -y python3-dev python3-numpy libffi-dev libsdl2-dev libosmesa6-dev
-apt-get install libav-tools
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get install -y cmake python3-pip gettext
+sudo apt-get install -y pkg-config zip g++ zlib1g-dev unzip python3 python
+sudo apt-get install -y python3-dev python3-numpy libffi-dev libsdl2-dev libosmesa6-dev
+sudo apt-get install libav-tools
 
 pip3 install torch
 pip3 install torchvision
@@ -24,8 +24,46 @@ cd ../rita
 
 export PYTHON_BIN_PATH=$(which python3)
 echo "PYTHON_BIN_PATH=$(which python3)" >> ~/.bashrc
-git clone https://github.com/SokolovRE/lab.git && cd lab
-bazel build --python_path=$(which python3) -c opt python/pip_package:build_pip_package
+git clone https://github.com/deepmind/lab.git && cd lab
+echo "
+# Description:
+#   Build rule for Python and Numpy.
+#   This rule works for Debian and Ubuntu. Other platforms might keep the
+#   headers in different places, cf. 'How to build DeepMind Lab' in build.md.
+cc_library(
+    name = \"python\",
+    hdrs = glob([
+        \"include/python3.6/*.h\",
+        \"lib/python3/dist-packages/numpy/core/include/numpy/*.h\",
+    ]),
+    includes = [
+        \"include/python3.6\",
+        \"lib/python3/dist-packages/numpy/core/include\",
+    ],
+    visibility = [\"//visibility:public\"],
+)
+" > python.BUILD
+cd python/pip_package
+echo "
+import setuptools
+REQUIRED_PACKAGES = [
+    'numpy >= 1.13.3',
+    'six >= 1.10.0',
+]
+setuptools.setup(
+    name='DeepMind Lab',
+    version='1.0',
+    description='DeepMind Lab',
+    long_description='',
+    url='https://github.com/deepmind/lab',
+    author='DeepMind',
+    packages=setuptools.find_packages(),
+    install_requires=REQUIRED_PACKAGES,
+    include_package_data=True)
+" > setup.py
+cd ../..
+
+bazel build --verbose_failures --python_path=$(which python3) -c opt python/pip_package:build_pip_package
 ./bazel-bin/python/pip_package/build_pip_package /tmp/dmlab_pkg
 pip3 install /tmp/dmlab_pkg/DeepMind_Lab-1.0-py3-none-any.whl --force-reinstall
 cd ..
